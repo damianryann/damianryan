4b70f053f460111610f8b8d84b2c788078abb606
Damian Ryan <damian@damianryan.co.uk>
2023-08-12 22:14:42 +0100

    Small update to hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,22 +13,41 @@ export default function Home() {
       <main>
         <Header title="This is Red Tower" />
         <div className="section-container">
-          <section className="image d-flex align-items-center">
-            <span className="text-white">It's time to party</span>
+          <section className="image d-flex align-items-end justify-content-start">
+            <div className="row">
+              <div className="col-md-12 col-lg-6">
+                <span className="display-1 text-white fw-bold bg-black">
+                  It's time to party with us at Notting Hill Carnival 2023
+                </span>
+              </div>
+            </div>
           </section>
           <section className="secondary">
-            <h1>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will
-              uncover many web sites still in their infancy. Various versions
-              have evolved over the years, sometimes by accident, sometimes on
-              purpose (injected humour and the like).
-            </h1>
+            <div className="row">
+              <div className="col-md-6">
+                <h1>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters
+                </h1>
+              </div>
+              <div className="col-md-6">
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
+                  Many desktop publishing packages and web page editors now use
+                  Lorem Ipsum as their default model text, and a search for
+                  'lorem ipsum' will uncover many web sites still in their
+                  infancy. Various versions have evolved over the years,
+                  sometimes by accident, sometimes on purpose (injected humour
+                  and the like).
+                </p>
+              </div>
+            </div>
           </section>
           <section className="primary">
             <h1>Go ahead...</h1>
